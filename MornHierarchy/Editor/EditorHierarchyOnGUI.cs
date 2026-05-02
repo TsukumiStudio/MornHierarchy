0c2dc7204ec62ae64c7fcb13c54af3aaceb67c6a
matsufriends <pikapikan0526@gmail.com>
2026-05-02 16:26:01 +0900

    fix: Line黒線を左端 (xMin=0) まで延長

--- MornHierarchy/Editor/EditorHierarchyOnGUI.cs
+++ MornHierarchy/Editor/EditorHierarchyOnGUI.cs
@@ -20,7 +20,7 @@ namespace MornHierarchy {
         }
         private static void DrawLineDecoration(Rect selectionRect) {
             var lineRect = selectionRect;
-            lineRect.xMin = 32;
+            lineRect.xMin = 0;
             lineRect.xMax = Mathf.Max(lineRect.xMax,EditorGUIUtility.currentViewWidth);
             var upper = lineRect;
             upper.yMax = upper.yMin + 2;
