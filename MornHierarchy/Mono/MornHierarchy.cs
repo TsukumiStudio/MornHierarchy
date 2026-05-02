b6cc07ab2f6f38c058adb5db5dc6dcdb68890e9a
matsufriends <pikapikan0526@gmail.com>
2026-05-02 16:31:41 +0900

    feat: ビルド時はクラス本体を空に、デフォルトアイコン Icon_Logic 同梱

--- MornHierarchy/Mono/MornHierarchy.cs
+++ MornHierarchy/Mono/MornHierarchy.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 namespace MornHierarchy {
     public class MornHierarchy : MonoBehaviour {
+#if UNITY_EDITOR
         public bool EnableColor = true;
         [ColorUsage(false)]public Color BackColor;
         public bool ApplyChildren;
         public Texture2D Icon;
         public bool IsLine;
-        #if UNITY_EDITOR
         private void OnValidate() {
             UnityEditor.EditorApplication.RepaintHierarchyWindow();
         }
-        #endif
+#endif
     }
 }
