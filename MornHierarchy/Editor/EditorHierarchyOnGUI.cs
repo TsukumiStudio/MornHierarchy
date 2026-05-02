019f0c01f6d02df86f48970e032246fd465d9f5d
matsufriends <pikapikan0526@gmail.com>
2026-05-02 16:09:49 +0900

    refactor: Tag表示削除、Labelをネイティブ準拠に修正

--- MornHierarchy/Editor/EditorHierarchyOnGUI.cs
+++ MornHierarchy/Editor/EditorHierarchyOnGUI.cs
@@ -11,7 +11,6 @@ namespace MornHierarchy {
             if(gameObject == null) return;
             DrawColor(selectionRect,gameObject);
             DrawLabel(selectionRect,gameObject);
-            DrawTag(instanceID,selectionRect,gameObject);
         }
         private static void DrawColor(Rect selectionRect,GameObject gameObject) {
             var hasDrawn = false;
@@ -66,45 +65,9 @@ namespace MornHierarchy {
         }
         private static void DrawLabel(Rect selectionRect,GameObject gameObject) {
             selectionRect.xMin += 18;
-            var style = new GUIStyle();
-            style.normal.textColor = gameObject.activeInHierarchy ? GUI.contentColor : Color.gray;
-            style.alignment        = TextAnchor.UpperLeft;
+            var style = new GUIStyle(EditorStyles.label);
+            style.normal.textColor = gameObject.activeInHierarchy ? EditorStyles.label.normal.textColor : Color.gray;
             EditorGUI.LabelField(selectionRect,gameObject.name,style);
-        }
-        private static void DrawTag(int instanceId,Rect selectionRect,GameObject gameObject) {
-            var tag = gameObject.tag;
-            if(tag == "Line") DrawLine(instanceId,selectionRect,gameObject);
-            var style = new GUIStyle();
-            selectionRect.xMax     -= 16;
-            selectionRect.xMin     += selectionRect.width - 80;
-            style.normal.textColor =  tag == "Untagged" ? Color.red : GUI.color;
-            style.alignment        =  TextAnchor.MiddleRight;
-            EditorGUI.LabelField(selectionRect,tag,style);
-        }
-        private static void DrawLine(int instanceID,Rect selectionRect,GameObject gameObject) {
-            //DrawBack
-            EditorGUI.DrawRect(selectionRect,GetBackGroundColor(instanceID,selectionRect));
-            selectionRect.xMin = 32;
-            
-            //UpperLine
-            var upperLineRect = selectionRect;
-            upperLineRect.yMax = upperLineRect.yMin + 2;
-            EditorGUI.DrawRect(upperLineRect,Color.black);
-
-            //LowerLine
-            var lowerLineRect = selectionRect;
-            lowerLineRect.yMin = lowerLineRect.yMax - 2;
-            EditorGUI.DrawRect(lowerLineRect,Color.black);
-
-            //Label
-            var style = new GUIStyle();
-            style.normal.textColor = GUI.contentColor;
-            style.alignment        = TextAnchor.UpperCenter;
-            EditorGUI.LabelField(selectionRect,gameObject.name,style);
-        }
-        private static Color GetBackGroundColor(int instanceID,Rect rect) {
-            if(Selection.Contains(instanceID)) return new Color32(44,93,134,255);
-            return rect.Contains(Event.current.mousePosition) ? new Color32(68,68,68,255) : new Color32(56,56,56,255);
         }
     }
 }
